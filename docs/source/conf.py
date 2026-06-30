c6698250c78b670004c380a390cf69d88fe1dfc8
Hubert <hubert.rybka@doctoral.uj.edu.pl>
2026-06-30 22:55:23 +0200

    update-docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'ADMET-Xspec'
+copyright = '2026, Rybka, Masztalerz'
+author = 'H. Rybka & K. Masztalerz'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
